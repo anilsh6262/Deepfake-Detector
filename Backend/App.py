ce9e40dd69e1b7101d7fe94ae51b0eed09fe2b47
Anilkumar <your-email@example.com>
2026-05-19 10:33:35 +0530

    Backend deployment ready

--- Backend/App.py
+++ Backend/App.py
@@ -1,19 +1,30 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
+import os
 
 from routes.photos import photos_bp
 from routes.scan import scan_bp
+from routes.logs import logs_bp
 
 app = Flask(__name__)
 
 CORS(app)
 
+# ROUTES
 app.register_blueprint(photos_bp, url_prefix="/photos")
 app.register_blueprint(scan_bp, url_prefix="/scan")
+app.register_blueprint(logs_bp, url_prefix="/logs")
+
 
 @app.route("/")
 def home():
-    return {"message": "Backend Running"}
+    return jsonify({
+        "message": "Backend Running"
+    })
+
+
+# DEPLOYMENT CONFIG
+port = int(os.environ.get("PORT", 5000))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=port)
